b0a087f61431e3eb8d754f51d0a65eefc97ed031
Sean H <sbhurley@crimson.ua.edu>
2023-10-06 15:06:33 -0500

    Established basic layout for Step 2, also removed unused unused import in Step 5

--- src/components/steps/Step2.js
+++ src/components/steps/Step2.js
@@ -1,8 +1,49 @@
+// Step 2 - Document Information
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 
-//This component is a placeholder for now to show that a component
-//representing a step can be paired into the FormStepper component
+const templateList = [
+  {
+    value: 'word',
+    label: 'Microsoft Word',
+  }
+];
+
+const styleGuideList = [
+  {
+    value: 'style1',
+    label: 'Style 1',
+  },
+  {
+    value: 'style2',
+    label: 'Style 2',
+  }
+];
+
+const docTypeList = [
+  {
+    value: 'type1',
+    label: 'Document Type 1',
+  },
+  {
+    value: 'type1',
+    label: 'Document Type 1',
+  }
+];
+
+const fontList = [
+  {
+    value: 'arial',
+    label: 'Arial',
+  },
+  {
+    value: 'timesnewroman',
+    label: 'Times New Roman',
+  }
+];
+
 export default function Step2() {
   return (
     <Box
@@ -14,6 +55,58 @@ export default function Step2() {
       autoComplete="off"
     >
       <div>
+        <h2>Document Information</h2>
+        <h3>Which template would you like to use?</h3>
+        <TextField
+          id="template-selector"
+          select
+          label="Select"
+        >
+          {templateList.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        
+        <h3>Choose Style Guide</h3>
+        <TextField
+          id="style-guide-selector"
+          select
+          label="Select"
+        >
+          {styleGuideList.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        
+        <h3>What is your document type?</h3>
+        <TextField
+          id="doc-type-selector"
+          select
+          label="Select"
+        >
+          {docTypeList.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        
+        <h3>Choose an approved font</h3>
+        <TextField
+          id="font-selector"
+          select
+          label="Select"
+        >
+          {fontList.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
       </div>
     </Box>
   );
--- src/components/steps/Step5.js
+++ src/components/steps/Step5.js
@@ -1,7 +1,6 @@
 // Step 5 - Additional Document Sections
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
