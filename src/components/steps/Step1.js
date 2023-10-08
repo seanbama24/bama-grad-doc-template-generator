095d8f52b2f23ade22979b62315f2f3fd24e3acd
Robert Melton <rsmelton@crimson.ua.edu>
2023-10-08 17:19:57 -0500

    Step 1 UI completed

--- src/components/steps/Step1.js
+++ src/components/steps/Step1.js
@@ -1,10 +1,23 @@
-import * as React from 'react';
+// import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import DatePicker from 'react-date-picker'
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+
+const degreeList = [
+  {value: 'computerScience', label: 'Computer Science'}
+];
 
 //This component is a placeholder for now to show that a component
 //representing a step can be paired into the FormStepper component
 export default function Step1() {
+
+  const [defenseDate, setdefenseDate] = useState(new Date());
+  const [graduationDate, setgraduationDate] = useState(new Date());
+
   return (
     <Box
       component="form"
@@ -14,74 +27,24 @@ export default function Step1() {
       noValidate
       autoComplete="off"
     >
-      <div>
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-        />
-        <TextField
-          id="outlined-textarea"
-          label="Multiline Placeholder"
-          placeholder="Placeholder"
-          multiline
-        />
-        <TextField
-          id="outlined-multiline-static"
-          label="Multiline"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-        />
-      </div>
-      <div>
-        <TextField
-          id="filled-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-          variant="filled"
-        />
-        <TextField
-          id="filled-textarea"
-          label="Multiline Placeholder"
-          placeholder="Placeholder"
-          multiline
-          variant="filled"
-        />
-        <TextField
-          id="filled-multiline-static"
-          label="Multiline"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-          variant="standard"
-        />
-        <TextField
-          id="standard-textarea"
-          label="Multiline Placeholder"
-          placeholder="Placeholder"
-          multiline
-          variant="standard"
-        />
-        <TextField
-          id="standard-multiline-static"
-          label="Multiline"
-          multiline
-          rows={4}
-          defaultValue="Default Value"
-          variant="standard"
-        />
+      <div style={{textAlign: 'center'}}>
+        <h2>Tell us about yourself</h2>
+        <h3>What is your full name?</h3>
+        <TextField></TextField>
+        <h3>What degree are you currently pursuing?</h3>
+        <TextField id="degree-selection" select label="Select">
+          {degreeList.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+        </TextField>
+        <h3>When is your anticipated defense date?</h3>
+        <DatePicker selected={defenseDate} value={defenseDate} onChange={(defenseDate) => setdefenseDate(defenseDate)} />
+        <p>Please select the month and year you will be defending.</p>
+        <h3>When is your anticipated graduation date?</h3>
+        <DatePicker selected={graduationDate} value={graduationDate} onChange={(graduationDate) => setgraduationDate(graduationDate)} />
+        <p>Please select the month and year you will be graduating.</p>
       </div>
     </Box>
   );
