061c2ec028cba52b84aaffe4fd0d10716d8870b0
Robert Melton <rsmelton@crimson.ua.edu>
2023-10-25 15:47:57 -0500

    Added code for Graduate School Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import * as React from 'react';
 import FormStepper from './components/FormStepper';
+import logo from './Logos/OfficLogo-Graduate.png';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          The University of Alabama Graduate School Thesis Document Generator
-        </p>
+      <img src={logo} width={500} height={100} alt="logo" style={{margin: '2em 5em 2em 5em'}}/>
+      <p>
+        The University of Alabama Graduate School Thesis Document Generator
+      </p>
       <FormStepper/>
     </div>
   );
