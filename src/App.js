c2e36159cf2082039f59aa74caebf2651884c7f1
Robert Melton <rsmelton@crimson.ua.edu>
2023-10-25 20:25:22 -0500

    Added picture next to logo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import * as React from 'react';
 import FormStepper from './components/FormStepper';
 import logo from './Logos/OfficLogo-Graduate.png';
+import picture from './Logos/WLAM.png';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} width={400} height={75} alt="logo" style={{margin: '2em 5em 2em 5em'}}/>
+      <img src={picture} width={500} height={75} alt="logo" style={{margin: '2em 5em 2em 5em'}}/>
       <p style={{margin: '2em 5em 2em 5em'}}>
         The University of <span style={{color: '#9E1B32'}}>Alabama</span> Graduate School Thesis Document Generator
       </p>
