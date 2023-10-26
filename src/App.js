4d4d1ca8fa8b483e5260f7eb73bc7866b65462f7
Robert Melton <rsmelton@crimson.ua.edu>
2023-10-26 15:06:29 -0500

    Changed height of grad school picture

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import picture from './Logos/WLAM.png';
 function App() {
   return (
     <div className="App">
-      <img src={logo} width={400} height={75} alt="logo" style={{margin: '2em 5em 2em 5em'}}/>
+      <img src={logo} width={500} height={75} alt="logo" style={{margin: '2em 5em 2em 5em'}}/>
       <img src={picture} width={500} height={75} alt="logo" style={{margin: '2em 5em 2em 5em'}}/>
       <p style={{margin: '2em 5em 2em 5em'}}>
         The University of <span style={{color: '#9E1B32'}}>Alabama</span> Graduate School Thesis Document Generator
