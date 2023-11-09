1d203efce77d6e85c23cce9fa640e61bb90649f5
Allan Elfe <allanelfe@Allans-MacBook-Pro.local>
2023-11-09 11:12:18 -0600

    Document gen page 1

--- src/generateDocument.js
+++ src/generateDocument.js
@@ -1,25 +1,98 @@
 import { saveAs } from "file-saver"
-import { Document, Packer, Paragraph, TextRun } from "docx";
+import { Document, Packer, Paragraph, TextRun, AlignmentType, HeadingLevel } from "docx";
+import { alignProperty } from "@mui/material/styles/cssUtils";
 
 export default function generateDocument() {
-    console.log("we da best")
     const doc = new Document({
         sections: [
             {
-                properties: {},
+                properties: {
+                },
                 children: [
                     new Paragraph({
                         children: [
-                            new TextRun("Hello World"),
                             new TextRun({
-                                text: "Foo Bar",
-                                bold: true,
+                                text: "[INSERT TITLE HERE, INVERTED PYRAMID FORM,",
+                                break: 4,
+                                size: 24
                             }),
                             new TextRun({
-                                text: "\tGithub is the best",
-                                bold: true,
+                                text: "DOUBLE-SPACING IN BETWEEN LINES]",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "by",
+                                break: 4,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "[AUTHOR NAME, FIRST M. LAST, OMIT TITLES AND DEGREES]",
+                                break: 4,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "[NAME OF COMMITTEE CHAIR, FIRST M. LAST, WITH TITLE]",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "[NAME OF CO-CHAIR (IF APPOINTED), FIRST M. LAST, WITH TITLE]",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "[OTHER COMMITTEE MEMBERS, FIRST M. LAST]",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "(OMIT TITLES AND DEGREES)",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "A DISSERTATION (OR THESIS)",
+                                break: 4,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "Submitted in partial fulfillment of the requirements",
+                                break: 4,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "for the degree of [Doctor of Philosophy, Doctor of Education, Master of Arts, Master of Science]",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "in the Department of [official Department Name]",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "in the Graduate School of",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "The University of Alabama",
+                                break: 2,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "TUSCALOOSA, ALABAMA",
+                                break: 4,
+                                size: 24
+                            }),
+                            new TextRun({
+                                text: "[YEAR OF PUBLICATION]",
+                                break: 4,
+                                size: 24
                             }),
                         ],
+                        alignment: AlignmentType.CENTER,
                     }),
                 ],
             },
