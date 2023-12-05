1443d296528553f40bed19c7f5bd058ce7af1c0a
Brandon Jones <bdjones15@crimson.ua.edu>
2023-12-05 14:21:59 -0600

    Added header and instructions to Step 5

--- src/components/steps/Step5.js
+++ src/components/steps/Step5.js
@@ -20,6 +20,8 @@ export default function Step5({ form, setForm }) {
       autoComplete="off"
     >
       <div>
+        <h1>Additional Document Sections</h1>
+        <h3>Check the box next to the additional document sections you would like to include. Please refer to the sample document and the Format Manual for an example of each additional field.</h3>
         <FormGroup>
           <FormControlLabel control={<Checkbox checked={form.includeDedication} onChange={(e) => setForm({...form, includeDedication: e.target.checked})} style={{color: '#9E1B32'}} />} label="Dedication" />
           <FormControlLabel control={<Checkbox checked={form.includeAbbreviationsAndSymbols} onChange={(e) => setForm({...form, includeAbbreviationsAndSymbols: e.target.checked})} style={{color: '#9E1B32'}}/>} label="Abbreviations and Symbols" />
@@ -27,7 +29,7 @@ export default function Step5({ form, setForm }) {
           <FormControlLabel control={<Checkbox checked={form.includeFigures} onChange={(e) => setForm({...form, includeFigures: e.target.checked})} style={{color: '#9E1B32'}}/>} label="Figures" />
           <FormControlLabel control={<Checkbox checked={form.includeIllustrations} onChange={(e) => setForm({...form, includeIllustrations: e.target.checked})} style={{color: '#9E1B32'}}/>} label="Illustrations" />
         </FormGroup>
-        <h3>Please refer to the sample document and the Format Manual for an example of each additional field.</h3>
+        {/* <h3></h3> */}
       </div>
     </Box>
   );
